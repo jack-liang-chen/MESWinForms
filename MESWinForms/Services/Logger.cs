f7ad4086976bbd9afc057d2696cbe46580a83e34
liang.chen <liang2.chen@ni.com>
2022-10-17 10:07:50 +0800

    logger.

--- MESWinForms/Services/Logger.cs
+++ MESWinForms/Services/Logger.cs
@@ -1,26 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Serilog;
+using System.IO;
 
 namespace MESWinForms.Services
 {
     public class Logger
     {
+        const string LOG_FILE = "MES.log";
+
         public Logger()
         {
+            if (File.Exists(LOG_FILE))
+                File.Delete(LOG_FILE);
 
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(LOG_FILE)
+                .CreateLogger();
         }
 
         public void Info(string message) 
         {
-            
+            Log.Information(message);
         }
 
         public void Error(string message)
         {
-
+            Log.Error(message);
         }
     }
 }
